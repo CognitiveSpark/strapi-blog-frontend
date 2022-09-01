1331a5fc7d516bbcbb7b8480fe8ce165b986be78
Andrii <paelesmer@gmail.com>
2022-09-01 14:06:02 +0300

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,7 +39,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     preloadingStrategy: PreloadAllModules,
-    relativeLinkResolution: 'legacy'
+    relativeLinkResolution: 'legacy',
+    useHash: true
   })],
   exports: [RouterModule]
 })
